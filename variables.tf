19c8fff51d34b0457ab5246b086c805976e65b3c
Bhargav-Dodda-07 <bhargavdodda969@gmail.com>
2026-02-09 21:21:04 +0530

    modules

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "ami_id" {
+    type = string
+    description = "this is the AMI ID used for instance creation"
+}
+
+variable "instance_type" {
+    type = string
+    description = "instance type used for ec2 creation"
+    validation {
+        condition = contains(["t3.micro", "t3.small", "t3.medium" ], var.instance_type)
+        error_message = "please select either t3.micro or t3.small or t3.medium"
+    }
+}
+
+variable "sg_ids" {
+    type = list
+}
+
+variable "tags" {
+    type = map
+    default = {}
+}
